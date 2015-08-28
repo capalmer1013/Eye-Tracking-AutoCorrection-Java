51d7fd1fc34c2cac418baa83963fd21b91bf9575
Chris Palmer <capalmer1013@gmail.com>
2015-08-28 15:47:38 -0400

    Wonderful UI

--- src/eyeTrackingAutocorrection/EyeTrackingAutoCorrection.java
+++ src/eyeTrackingAutocorrection/EyeTrackingAutoCorrection.java
@@ -4,6 +4,9 @@ public class EyeTrackingAutoCorrection {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        ChooseFile window = new ChooseFile();
+        window.setDefaultCloseOperation(ChooseFile.EXIT_ON_CLOSE);
+        window.setVisible(true);
     }
     
 }
